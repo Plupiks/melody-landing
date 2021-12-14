731a7ad4af5fea1403df3cb022a12ff08ca11d1a
Vitaliy Vasylykiv <92222356+Plupiks@users.noreply.github.com>
2021-12-14 21:22:35 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,14 +1,10 @@
 const houseFloor = document.querySelector('.house-img');
 
 houseFloor.addEventListener('click', function (e) {
-  // houseFloor.classList.toggle("house-img--active");
+  var floors = document.querySelectorAll(".house-floor");
   var floorActive = e.target;
-  floorActive.classList.toggle("house-img--active");
+  Array.from(floors).forEach(i => {
+    i.classList.remove("house-img--active");
+  })
+  floorActive.classList.add("house-img--active");
 })
-
-
-// houseFloor.addEventListener('click', function (e) {
-//   var houseFloorActive = e.target.dataset;
-//   houseFloor.style.opacity = 0.75;
-//   // console.log(e.target.dataset);
-// })
