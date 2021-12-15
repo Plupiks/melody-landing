9aec6f3f0c01047560676585084d95861102cc1f
Vitaliy Vasylykiv <92222356+Plupiks@users.noreply.github.com>
2021-12-16 00:47:55 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-var floors = document.querySelectorAll(".house-floor");
-const floorCountNumb = document.querySelector('.hero--value');
+const floors = document.querySelectorAll(".house-floor");
+const floorCountNumb = document.querySelector(".hero--value");
+const arrowUp = document.querySelector(".hero--arrow-up");
+const arrowDown = document.querySelector(".hero--arrow-down");
 
-
+// При клике на этаж
 floors.forEach(e => {
 
   e.addEventListener('click', function (e) {
@@ -16,4 +18,26 @@ floors.forEach(e => {
     var floorCount = e.target.dataset.floor;
     floorCountNumb.textContent = floorCount;
   })
+})
+
+arrowUp.addEventListener('click', () => {
+  let value = parseInt(floorCountNumb.innerHTML);
+
+  if (value < 17) {
+    floors[value - 1].classList.remove('house-img--active');
+    value++;
+    floorCountNumb.innerHTML = value;
+    floors[value - 1].classList.add('house-img--active');
+  }
+})
+
+arrowDown.addEventListener('click', () => {
+  let value = parseInt(floorCountNumb.innerHTML);
+
+  if (value > 1) {
+    floors[value - 1].classList.remove('house-img--active');
+    value--;
+    floorCountNumb.innerHTML = value;
+    floors[value - 1].classList.add('house-img--active');
+  }
 })
